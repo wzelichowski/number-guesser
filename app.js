86bad76cacb95e57e0b8baef6d4a6dec65ae6d67
Wojciech Zelichowski <wzelichowski@gmail.com>
2018-09-23 23:07:13 +0200

    Add basic JS logic with gamover final points

--- app.js
+++ app.js
@@ -18,10 +18,33 @@ maxNum.textContent = max;
 guessBtn.addEventListener('click', function() {
   let guess = parseInt(guessInput.value);
   if(isNaN(guess) | guess < min | guess > max) {
-    setMessage(`Please enter a number between ${min} and ${max}.`);
+    setMessage(`Please enter a number between ${min} and ${max}.`, 'red');
   }
+if(guess === winningNum) {
+  gameOver(true, `${winningNum} is correct! You win!`);
+} else {
+  guessesLeft -= 1;
+  if(guessesLeft === 0) {
+    gameOver(false, `Game over! You lost. The correct number was ${winningNum}...`);
+  } else {
+    setMessage(`Guess is not correct! Guesses left: ${guessesLeft}`, 'red');
+    guessInput.style.borderColor = 'red';
+    guessInput.value = '';
+  }
+}
+
 })
 
-function setMessage(msg) {
+function setMessage(msg, color) {
   message.textContent = msg;
+  message.style.color = color;
+}
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? color = 'green' : color = 'red';
+  guessInput.disabled = true;
+  guessInput.style.borderColor = 'green';
+  message.style.color = color;
+  setMessage(msg);
 }
